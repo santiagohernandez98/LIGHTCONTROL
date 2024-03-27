8852ec3b6502679e0c968c062100a95f9a58e68a
santiagohernandez98 <106284148+santiagohernandez98@users.noreply.github.com>
2024-03-26 23:34:16 -0500

    cambios en el panel de menu y ajustes

--- CapaModelo/Eleccion.cs
+++ CapaModelo/Eleccion.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ /*   pqrs  modelo
+  */ 
+  
 namespace CapaModelo
 {
     public class Eleccion
     {
-        public int IdEleccion { get; set; }
-        public string Descripcion { get; set; }
-        public string Cargo { get; set; }
-        public DateTime FechaRegistro { get; set; }
-        public Usuario oUsuario { get; set; }
-        public bool Activo { get; set; }
+        public int id { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public string canal { get; set; }
+        public string tipo_pqrs { get; set; }
+        public string referencia { get; set; }
+        public string documento { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+        public string correo_electronico { get; set; }
+        public string descripcion_afectacion { get; set; }
+        public string tipo_alumbrado { get; set; }
+        public string estado { get; set; }
+        public string oberservaciones { get; set; }
     }
 }
